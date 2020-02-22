e6271557e0699120fa711f9e2694bf573074119f
Richard Anabeto Opoku <anabeto93@gmail.com>
2020-02-22 15:20:07 +0000

    can authenticate user from API

--- app/Http/Controllers/API/Auth/AuthenticateUserController.php
+++ app/Http/Controllers/API/Auth/AuthenticateUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 use App\Repositories\User\UserInterface;
 
 class AuthenticateUserController extends Controller
@@ -24,6 +25,16 @@ class AuthenticateUserController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if (auth()->attempt($credentials)) {
+            $token = auth()->user()->createToken('MyApp')->accessToken;
+            return response()->json(['user' => new UserResource(\App\Models\User::find(auth()->user()->id)), 'access_token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'Invalid credentials.'], 401);
+        }
     }
 }
